35995e93b40db10221067da229ae2abe6ea8c5a4
Tarık  Bayram <tkbayram7@gmail.com>
2022-08-01 12:16:05 +0300

    Updated : SliderCar Component

--- finish/src/App.js
+++ finish/src/App.js
@@ -2,17 +2,25 @@ import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/Header";
 import style from "./App.module.scss";
 import Information from "./components/Information/Information";
+import MainCar from "./components/MainCar/MainCar";
 
 function App() {
   return (
     <div className={style.App}>
       <div className={style.App__main}>
-      <BrowserRouter>
-          <Header/>
-      </BrowserRouter>
+        <section>
+          <BrowserRouter>
+            <Header />
+          </BrowserRouter>
+        </section>
+        <section>
+          <Information />
+          <MainCar />
+        </section>
+        <section>
+          
+        </section>
       </div>
-
-      <Information/>
     </div>
   );
 }
